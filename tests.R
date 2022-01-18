f441049ea4e7f7b03edaabcc10c2789acf56e0fe
Grady Weyenberg <gradysw@hawaii.edu>
2022-01-17 23:51:38 -1000

    first wrangling questions

--- tests.R
+++ tests.R
@@ -1,2 +1,8 @@
 ## execute all code in the solution file
-try(knitr::knit(text=readLines('PSxx_solution.Rmd')))
+try(knitr::knit(text=readLines('PS03_solution.Rmd')))
+
+test_that("Basic syntax questions",{
+  expect_equal(nrow(dallas_sub), 24)
+  expect_vector(dallas_sub$prct_sold, numeric())
+  expect_equal(dim(dallas_summary), c(12,2))
+})
