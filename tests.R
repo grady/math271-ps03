f5a898a6eda276633f35659567e8cebdd881d814
Grady Weyenberg <gradysw@hawaii.edu>
2024-01-04 14:44:05 -1000

    update to autograding

--- tests.R
+++ tests.R
@@ -1,8 +1,10 @@
-## execute all code in the solution file
-try(knitr::knit(text=readLines('PS03_solution.Rmd')))
+test_that("<5>Solution file exists and knits",{
+  expect_true(file.exists("solution.Rmd"))
+  suppressWarnings(expect_error(knitr::knit("solution.Rmd", quiet = TRUE, envir=globalenv()), NA))
+})
 
-test_that("Basic syntax questions",{
-  expect_equal(nrow(dallas_sub), 24)
-  expect_vector(dallas_sub$prct_sold, numeric())
-  expect_equal(dim(dallas_summary), c(12,2))
+test_that("<4> Dallas subset",{
+  expect_error(expect_equal(nrow(dallas_sub), 24),NA)
+  expect_error(expect_vector(dallas_sub$prct_sold, numeric()), NA)
+  expect_error(expect_equal(dim(dallas_summary), c(12,2)), NA)
 })
