f39bda814352222bde25365fb2db1898e4c2f563
Grady Weyenberg <gradysw@hawaii.edu>
2024-01-21 15:48:09 -1000

    some fixes to autograder script

--- tests.R
+++ tests.R
@@ -3,8 +3,14 @@ test_that("<5>Solution file exists and knits",{
   suppressWarnings(expect_error(knitr::knit("solution.Rmd", quiet = TRUE, envir=globalenv()), NA))
 })
 
-test_that("<4> Dallas subset",{
+test_that("<1> Dallas subset", {
   expect_error(expect_equal(nrow(dallas_sub), 24),NA)
+})
+
+test_that("<1> Percent sold column", {
   expect_error(expect_vector(dallas_sub$prct_sold, numeric()), NA)
+})
+
+test_that("<2> Dallas summary data frame", {
   expect_error(expect_equal(dim(dallas_summary), c(12,2)), NA)
 })
